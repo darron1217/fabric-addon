eaf8786413f1e5bd6931afc77b119a34c380b39b
Darron Park <darron1217@gmail.com>
2018-07-24 10:27:37 +0900

    Update FixedTextbox to support realtime editing

--- src/fixed-textbox.js
+++ src/fixed-textbox.js
@@ -5,21 +5,50 @@
  */
 fabric.FixedTextbox = fabric.util.createClass(fabric.Textbox, {
   type: 'fixed-textbox',
-  initialize: function(text, options) {
-    this.callSuper('initialize', text, options);
+
+  /**
+   * Properties which when set cause object to change dimensions
+   * @type Object
+   * @private
+   */
+  _dimensionAffectingProps: fabric.Textbox.prototype._dimensionAffectingProps.concat('height'),
+
+  /**
+   * initDimensions without height setting
+   */
+  _initDimensions: function() {
+    this.isEditing && this.initDelayedCursor();
+    this.clearContextTop();
+    this._clearCache();
+    // clear dynamicMinWidth as it will be different after we re-wrap line
+    this.dynamicMinWidth = 0;
+    // wrap lines
+    this._styleMap = this._generateStyleMap(this._splitText());
+    // if after wrapping, the width is smaller than dynamicMinWidth, change the width and re-wrap
+    if (this.dynamicMinWidth > this.width) {
+      this._set('width', this.dynamicMinWidth);
+    }
+    if (this.textAlign.indexOf('justify') !== -1) {
+      // once text is measured we need to make space fatter to make justified text.
+      this.enlargeSpaces();
+    }
+    // clear cache and re-calculate height
+    // this.height = this.calcTextHeight();
+    this.saveState({ propertySet: '_dimensionAffectingProps' });
+  },
+
+  initDimensions: function() {
+    this._initDimensions();
 
     // If fontResizing mode enabled
     // 폰트 리사이징 모드가 설정되어 있다면
     if(this.fontResizing) {
+      this.height = this.maxHeight;
       if(!this.originFontSize) this.originFontSize = this.fontSize;
-      if(!this.maxHeight) this.maxHeight = this.height;
-      if(this.height > this.maxHeight) {
-        for(var i in this.textLines) {
-          var line = this.textLines[i];
-          if(this.textLines.length > i + 1) this.text = this.text.replace(line, line+'\n');
-          this.text = this.text.replace(/\n\s/g, "\n");
-        }
-        this.fontSize = (this.originFontSize / (this.height / this.maxHeight));
+      // 높이구하기
+      var textHeight = this.calcTextHeight();
+      if(textHeight > this.maxHeight) {
+        this.fontSize = this.fontSize / (textHeight / this.maxHeight);
       }
     }
     else {
@@ -27,6 +56,7 @@ fabric.FixedTextbox = fabric.util.createClass(fabric.Textbox, {
       // 아니라면, 넘치는 글자는 자름
     }
   },
+
 });
 
 fabric.FixedTextbox.fromObject = function(object, callback) {
